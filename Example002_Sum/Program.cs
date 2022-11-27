a8fc14a679113906e856f407f01a2fc2e3346239
EduardSh <freedom886@mail.ru>
2022-11-27 17:23:46 +0500

    Add folder with Sum

---
+++ Example002_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int namberA = new Random().Next(1,10);
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
+Console.WriteLine(namberA + namberB);
