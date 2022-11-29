c36e12dce6ec27651173ea031872aeb937f1fce1
EduardSh <freedom886@mail.ru>
2022-11-30 00:53:39 +0500

    Add home work 002

---
+++ Home_Work_002/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7, 44 5 78 -> 78, 22 3 9 -> 22
+Console.WriteLine("Введите три числа через пробел");
+string s = Console.ReadLine() ?? "";
+string[] s_arr = s.Split();
+int max_value=0;
+int index=0;
+while(index <3)
+{
+  if (int.Parse(s_arr[index])>max_value)
+  {
+        max_value= int.Parse(s_arr[index]);
+  }  
+  index++;
+}
+Console.WriteLine($" Max= {max_value}");
+
