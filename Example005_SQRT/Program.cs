8853cafd021e1b4cfb9263e693223121ab123c16
EduardSh <freedom886@mail.ru>
2022-11-30 00:13:13 +0500

    add all 3 objcet

--- Example005_SQRT/Program.cs
+++ Example005_SQRT/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.WriteLine("Enter number:");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+string[] week = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine(week[num - 1]);
