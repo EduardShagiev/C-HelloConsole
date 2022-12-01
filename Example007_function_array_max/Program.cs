2166531fb53af1829fdf0da494de2bca40dcdb09
EduardSh <freedom886@mail.ru>
2022-12-01 21:24:12 +0500

    edit example007

--- Example007_function_array_max/Program.cs
+++ Example007_function_array_max/Program.cs
@@ -4,12 +4,19 @@ Console.WriteLine("Input value by space ");
 string s = Console.ReadLine()?? "";
 string[] sarr= s.Split();
 int[] valarr = new int[sarr.Length];
+
 for (int i = 0; i < sarr.Length; i++)
 {
-    valarr[i] = int.Parse((sarr[i]));
-   // Console.Write(valarr[i]+ ", ");
+    bool result = int.TryParse(sarr[i], out valarr[i]);
+    if (result)
+    {    
+        Console.WriteLine("Операция конвертации успешна");
+    }
+    else
+    {  
+        Console.WriteLine("Не удалось конвертировать");
+    }
 }
-
 int funserchmaxval(int[] x)
 {
 int max_value=0;
@@ -26,3 +33,9 @@ return max_value;
 }
 
 Console.WriteLine("Max value =" + funserchmaxval(valarr));
+
+   // for (int i = 0; i < sarr.Length; i++)
+   // {
+   // valarr[i] = int.Parse((sarr[i]));
+   // Console.Write(valarr[i]+ ", ");
+   //  }
