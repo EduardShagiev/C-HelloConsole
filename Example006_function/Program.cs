2543103e83a3dd50d9e7667d38ea0393f815766d
EduardSh <freedom886@mail.ru>
2022-12-01 14:47:19 +0500

    Add new expample program

---
+++ Example006_function/Program.cs
@@ -0,0 +1,26 @@
+﻿// double f(double x)
+// {
+//     double result_x = x * x + 1;
+//     return result_x;
+// }
+
+//Console.WriteLine(f(23.5));
+
+int[] numbers = { 7, 2, 6, 4, 5 };
+foreach (int i in numbers)
+{
+    Console.Write(i + ", ");
+}
+
+char[] gender = {'m','f','m','m','m','f','f','m','m','f'};
+int male = 0, female = 0;
+foreach (char g in gender)  
+    {
+         if (g == 'm')
+            male++;
+        else if (g =='f')
+            female++;
+    }
+Console.WriteLine(" ");            
+Console.WriteLine("Number of male = {0}", male);
+Console.WriteLine("Number of female = {0}", female);
---
+++ Example007_function_array_max/Program.cs
@@ -0,0 +1,28 @@
+﻿// Функция находения максимального значения в масиве целых чисел
+
+Console.WriteLine("Input 7 value by space ");
+string s = Console.ReadLine()?? "";
+string[] sarr= s.Split();
+int[] valarr = new int[7];
+for (int i = 0; i < sarr.Length; i++)
+{
+    valarr[i] = int.Parse((sarr[i]));
+   // Console.Write(valarr[i]+ ", ");
+}
+
+int functionmax(int[] x)
+{
+int max_value=0;
+int index=0;
+while(index <7)
+{
+  if (x[index]>max_value)
+  {
+        max_value= x[index];
+  }  
+  index++;
+ }
+return max_value;
+}
+
+Console.WriteLine("Max value =" + functionmax(valarr));
