e78fd1e471f8e472eee933ee74f4a05e03d919d9
EduardSh <freedom886@mail.ru>
2022-12-04 14:58:47 +0500

    Add Home work 005 class 2

---
+++ Exapmle009_class_2/Program.cs
@@ -0,0 +1,6 @@
+﻿void ComperNamber(int x)
+{
+ if (x%7==0 && x%23==0) Console.WriteLine($"Число {x} кратно 7 и 23 ");
+ else Console.WriteLine("Не кратно");
+}
+ComperNamber(161);
---
+++ Home_Work_005/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5 782 -> 8 918 -> 1
+
+Console.WriteLine("Ввидите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondNumber(int value)
+{
+    int result = (value%100)/10; 
+    return result; 
+} 
+Console.WriteLine(SecondNumber(number));
