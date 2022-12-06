ad05148bf4966805174c368e8201dcd83e58d082
EduardSh <freedom886@mail.ru>
2022-12-07 01:56:16 +0500

    added Home Works 009 - 011

--- Home_Work_009/Program.cs
+++ Home_Work_009/Program.cs
@@ -2,28 +2,31 @@
 // пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет 12821 -> да 23432 -> да
 Console.WriteLine("Введите число");
-string s = Console.ReadLine()??"";
+string s = Console.ReadLine() ?? "";
 char[] str = s.ToCharArray();
 
 void CheckPalim(char[] value)
 {
-  int len=value.Length;
-  int index=0;
-  bool Palindrome=true;
-  int last = len-1;
-  while (index<len)
-  {
-   
-    if (str[index]==str[last])
+    int len = value.Length;
+    int first = 0;
+    int last = len - 1;
+    bool Palindrome = true;
+    while (first < last)
     {
-        last=last-1;  
+        if (str[first] == str[last])
+        {
+            first = first + 1;
+            last = last - 1;
+        }
+        else
+        {
+            Palindrome = false;
+            break;
+        }
     }
-    else Palindrome = false;
-    index++;
-  }
-  if (Palindrome) {Console.Write ("Чиcло палиндром");}
-  else  {Console.Write ("Чиcло не палиндром");}
+    if (Palindrome) { Console.Write("Чиcло палиндром"); }
+    else { Console.Write("Чиcло не палиндром"); }
 }
 
- CheckPalim(str);
+CheckPalim(str);
 
