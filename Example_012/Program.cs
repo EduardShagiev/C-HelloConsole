aa34d0456c0007d6759e3a2ea5e8247ef724aaef
EduardSh <freedom886@mail.ru>
2022-12-06 23:27:39 +0500

    add new folders from third lesson

---
+++ Example_012/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число 
+//(N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void SQRT(int value)
+{
+  for (int i = 1; i <= value; i++)
+  {
+    int result = i*i; 
+    Console.Write( result + " ");
+  }  
+}
+SQRT(N);
