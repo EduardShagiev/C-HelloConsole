79834efb3d3348ba7516530ac836966d99810489
EduardSh <freedom886@mail.ru>
2022-12-11 21:44:43 +0500

    Add

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");  
+﻿// Console.WriteLine("Введите числа через запятую");
+// string ins=Console.ReadLine()??"";
+// void inputarr(string str)
+// {
+//     int[]  arr = new int[8];
+//     string[] s=str.Split(",");
+//     int i=0;
+//     Console.Write("[");
+//     while(i<s.Length)
+//     {
+//         arr[i]=Convert.ToInt32(s[i]);
+//         Console.Write("{0},", arr[i]);
+//         i++;
+//     }
+//     Console.Write("]");
+// }
+// inputarr(ins);
+
+Random ramdom = new Random();
+int[,,] array = new int[3,3,3];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{       
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i,j,k]= ramdom.Next(100);
+        }
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{    Console.WriteLine("Page"+ (i+1));   
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            Console.Write(array[i,j,k]+" ");  
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("=====================");
+}
