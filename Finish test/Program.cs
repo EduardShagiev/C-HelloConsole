81b2f846b50d6dd25562491bfd2366c9468fc95b
EduardSh <freedom886@mail.ru>
2023-02-12 16:38:25 +0500

    Add programm

--- Finish test/Program.cs
+++ Finish test/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine() ?? "";
+M = stroka.Split();
+string[] result = new string[M .Length];
+int realSize = 0;   
+ foreach (var value in M )
+        {
+            if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+        }
+ Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
